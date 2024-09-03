72f0414354d7924a66320e4c2ab54837fd31723e
DESKTOP-0DUI2NJ\user <jungmin7315@kakao.com>
2024-09-03 22:54:51 +0900

    messageService CRUD 작성

--- ChannelService/src/main/java/com/bdool/channelservice/model/repository/ChannelProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bdool.channelservice.model.repository;
-
-import com.bdool.channelservice.model.entity.ChannelProfileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChannelProfileRepository extends JpaRepository<ChannelProfileEntity, Long> {
-}
