913374021bd94292fe83ff5d4851cd36b9058425
gusdn0413 <gusdn0413@naver.com>
2024-09-06 00:13:26 +0900

    initial commit

---
+++ member-hub-service/src/main/java/com/bdool/memberhubservice/MemberHubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bdool.memberhubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberHubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberHubServiceApplication.class, args);
+	}
+
+}
