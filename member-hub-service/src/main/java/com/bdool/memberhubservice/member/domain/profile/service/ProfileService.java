a7bd9eae2fea307b8bbfbabf4bd8632bd86b2a92
gusdn0413 <gusdn0413@naver.com>
2024-09-24 20:34:49 +0900

    리팩토링

--- member-hub-service/src/main/java/com/bdool/memberhubservice/member/domain/profile/service/ProfileService.java
+++ member-hub-service/src/main/java/com/bdool/memberhubservice/member/domain/profile/service/ProfileService.java
@@ -3,6 +3,7 @@ package com.bdool.memberhubservice.member.domain.profile.service;
 
 import com.bdool.memberhubservice.member.domain.profile.entity.Profile;
 import com.bdool.memberhubservice.member.domain.profile.entity.model.ProfileModel;
+import com.bdool.memberhubservice.member.domain.profile.entity.model.ProfileResponse;
 import com.bdool.memberhubservice.member.domain.profile.entity.model.ProfileUpdateRequest;
 
 import java.util.List;
@@ -18,6 +19,8 @@ public interface ProfileService {
     List<Profile> findAll();
 
     long count();
+
+    List<ProfileResponse> findByWorkspaceId(Long workspaceId);
 
     boolean existsById(Long profileId);
 
