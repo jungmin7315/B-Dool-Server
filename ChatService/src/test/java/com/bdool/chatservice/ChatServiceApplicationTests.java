861041756136a003abe3a3f7fa096bad3b230188
DESKTOP-0DUI2NJ\user <jungmin7315@kakao.com>
2024-09-27 12:23:20 +0900

    commit

--- ChatService/src/test/java/com/bdool/chatservice/ChatServiceApplicationTests.java
+++ ChatService/src/test/java/com/bdool/chatservice/ChatServiceApplicationTests.java
@@ -2,27 +2,57 @@ package com.bdool.chatservice;
 
 import com.bdool.chatservice.model.Enum.SessionType;
 import com.bdool.chatservice.model.domain.SessionModel;
+import com.bdool.chatservice.model.entity.MessageEntity;
+import com.bdool.chatservice.model.repository.MessageRepository;
+import com.bdool.chatservice.service.MessageService;
 import com.bdool.chatservice.service.SessionService;
+import com.opencsv.CSVReader;
+import org.apache.kafka.common.protocol.Message;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileReader;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootTest
 class ChatServiceApplicationTests {
 
-    SessionService sessionService;
+    @Autowired
+    private MessageRepository messageRepository;
 
     @Test
     void contextLoads() {
-        SessionModel session = new SessionModel();
-        session.setStartAt(LocalDateTime.parse("2024-09-06T04:48:31.862Z"));
-        session.setEndAt(LocalDateTime.parse("2024-09-06T04:48:31.862Z"));
-        session.setProfileId(1L);
-        session.setSessionType(SessionType.valueOf("VIDEO_CONFERENCE"));
-        session.setChannelMemberId(UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6"));
-        sessionService.save(session);
+
+        List<MessageEntity> messages = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(""))) {
+            String[] nextLine;
+            reader.readNext(); // 헤더 스킵
+            while ((nextLine = reader.readNext()) != null) {
+                MessageEntity message = MessageEntity.builder()
+                        .messageId(UUID.fromString(nextLine[0])) // messageId
+                        .channelId(UUID.fromString(nextLine[1])) // channelId
+                        .content(nextLine[2]) // content
+                        .sendDate(LocalDateTime.parse(nextLine[3], DateTimeFormatter.ISO_DATE_TIME)) // sendDate
+                        .isEdited(Boolean.parseBoolean(nextLine[4])) // isEdited
+                        .isDeleted(Boolean.parseBoolean(nextLine[5])) // isDeleted
+                        .parentMessageId(nextLine[6].isEmpty() ? null : UUID.fromString(nextLine[6])) // parentMessageId
+                        .participantId(UUID.fromString(nextLine[7])) // participantId
+                        .fileUrl(nextLine[8].isEmpty() ? null : nextLine[8]) // fileUrl
+                        .build();
+
+                messages.add(message);
+            }
+            // MongoDB에 데이터 저장
+            messageRepository.saveAll(messages);
+            System.out.println("CSV 데이터가 MongoDB에 성공적으로 삽입되었습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
