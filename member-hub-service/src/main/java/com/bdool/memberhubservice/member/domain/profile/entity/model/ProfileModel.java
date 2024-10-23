28bd85b44e80811c2425c41d2e9613d8eda11de4
gusdn0413 <gusdn0413@naver.com>
2024-10-23 15:05:54 +0900

    일정 초대에 대한 알림

--- member-hub-service/src/main/java/com/bdool/memberhubservice/member/domain/profile/entity/model/ProfileModel.java
+++ member-hub-service/src/main/java/com/bdool/memberhubservice/member/domain/profile/entity/model/ProfileModel.java
@@ -1,20 +1,13 @@
 package com.bdool.memberhubservice.member.domain.profile.entity.model;
 
-import com.bdool.memberhubservice.member.domain.member.entity.Member;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.Data;
-
-import java.util.Date;
 
 @Data
 public class ProfileModel {
 
     private String name; // 이름
     private String nickname; // 별명
-    private String profilePictureUrl; // 프로필 이미지 URL
-    private Boolean isOnline = true;
+    private String profileImgUrl; // 프로필 이미지 URL
+    private Boolean isOnline;
     private Long workspaceId;
 }
